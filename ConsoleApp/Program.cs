1b88cff1c2474e261ff6bfd39da681bdd3a99311
rutvikramanuj-cloud <rutvikramanuj@gmail.com>
2025-08-03 20:56:06 +0530

    Merge pull request #1 from rutvikramanuj-cloud/Feature/65321_CustomerName_Implementation_dev
    Feature/65321 customer name implementation dev

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+CustomerService customer = new();
+customer.GetCustomerName(); // this line will show the customer name from the customer service
